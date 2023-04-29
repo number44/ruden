7fec6fd00ca98a653e44d85cb3228b3f12dbeb67
number44 <daniel.lesiewicz.dev@gmail.com>
2023-04-29 09:06:42 +0200

    v.1.0

--- src/components/PigMap.tsx
+++ src/components/PigMap.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Map, Marker } from "pigeon-maps";
+import { Map, Marker, Overlay } from "pigeon-maps";
 
 export function PigMap() {
   return (
-    <Map height={300} defaultCenter={[51.7963234, 19.4386925]} defaultZoom={11}>
-      <Marker width={50} anchor={[51.7963234, 19.4386925]} />
+    <Map height={300} defaultCenter={[51.7963234, 19.4386925]} defaultZoom={15}>
+      {/* <Marker width={50} anchor={[51.7963234, 19.4386925]} /> */}
+      <Overlay anchor={[51.7963234, 19.4386925]} offset={[20, 40]}>
+        <img src="marker-white.png" width={40} alt="" />
+      </Overlay>
     </Map>
   );
 }
