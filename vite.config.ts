d6be11b25b9c95a3e3d3eecdc952d67da2ef6780
number44 <daniel.lesiewicz.dev@gmail.com>
2023-05-05 09:23:06 +0200

    ver 9.7

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+const root = __dirname;
+const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   build: {
     manifest: true,
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        en: resolve(root, "en.html"),
+      },
+    },
   },
   plugins: [react()],
 });
